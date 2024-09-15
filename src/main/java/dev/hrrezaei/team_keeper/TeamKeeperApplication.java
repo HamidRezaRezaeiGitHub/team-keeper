cda1703920abc38f4eee769ffb4171a783606d6f
Hamidreza Rezaei <hamidreza74hrr@yahoo.com>
2024-09-15 10:22:23 -0400

    Initial Spring boot setup

---
+++ src/main/java/dev/hrrezaei/team_keeper/TeamKeeperApplication.java
@@ -0,0 +1,13 @@
+package dev.hrrezaei.team_keeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamKeeperApplication.class, args);
+	}
+
+}
